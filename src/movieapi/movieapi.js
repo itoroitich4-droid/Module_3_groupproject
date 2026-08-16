88d16752698babd2874c42bb7c5e3a1fa9bc7f7b
itoroitich4-droid <itoroitich4@gmail.com>
2026-08-16 21:58:00 +0300

    L

--- src/movieapi/movieapi.js
+++ src/movieapi/movieapi.js
@@ -1,4 +1,4 @@
-const API_KEY = "your_tmdb_api_key"; 
+const API_KEY = "6a49a896e1a266bc4725c57c0c714343"; 
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const fetchPopularMovies = async () => {
